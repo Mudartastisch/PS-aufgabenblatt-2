c409cc1ab3d634c6d079fd3cc5d90d18cf1b940f
Kai Lorenz <kai.lorenz@uni-weimar.de>
2019-04-27 14:49:29 +0200

    created and tested vec2.hpp (2.2)

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
 
+TEST_CASE("test_vec2", "[Vec2]") {
+  Vec2 a ; // requires that 0.0f == a.x and 0.0f == a.y
+  REQUIRE(0.0f == a.x);
+  REQUIRE(0.0f == a.y);
+  Vec2 b {5.1f, -9.3f }; /* requires that 5.1f == approx(a.y) and -9.3f ==approx(b.y) */
+  //copy pasting from task sheet here, I know the line above is bs
+  REQUIRE(5.1f == Approx(b.x));
+  REQUIRE(-9.3f == Approx(b.y));
+
+}
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
---
+++ source/vec2.hpp
@@ -0,0 +1,11 @@
+# ifndef VEC2_HPP
+# define VEC2_HPP
+
+// Vec2 data type definition
+
+struct Vec2
+{
+    float x = 0.0f, y = 0.0f;
+};
+
+# endif // VEC2_HPP
